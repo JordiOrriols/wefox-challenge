ffb8e250c0d23455d90bc08b132e6a270e6deb76
Jordi Orriols <info@jordiorriols.cat>
2022-08-17 09:57:47 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@
  */
 
 import React, { useEffect, useState } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import styled from 'styled-components';
 
